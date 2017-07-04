7e116890fb7c6bbe53bb4061a1f491e74cf44022
FITZ-SPECTRE\Fitz <fitzsturgill@gmail.com>
2017-07-04 16:04:35 -0400

    simplified multi stim Bpod protocol

--- PhotoStim_multi/PhotoStim_multi.m
+++ PhotoStim_multi/PhotoStim_multi.m
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %}
 function PhotoStim_multi
 % PhotoStim w/ multiple frequencies for optogenetic tagging
-% Written by Hyun-Jae Pi 11/2014.
 
 global BpodSystem
 PulsePal;
@@ -32,76 +31,40 @@ TrialTypes=repmat([1 2 3 4 5],1,ceil(MaxTrials/5));
 BpodSystem.Data.TrialTypes = []; % The trial type of each trial completed will be added here.
 BpodSystem.Data.PulsePalProgram = {}; % the pulse pal program used will be deposited here
 
-%% Initialize plots
-BpodSystem.ProtocolFigures.OutcomePlotFig = figure('Position', [400 600 1000 200],'Name','Outcome plot','numbertitle','off', 'MenuBar', 'none', 'Resize', 'off');
-BpodSystem.GUIHandles.OutcomePlot = axes('Position', [.075 .3 .89 .6]);
-OutcomePlot_PhotoStim_multi(BpodSystem.GUIHandles.OutcomePlot,'init',2-TrialTypes,MaxTrials);
 
 %% Main trial loop
 for currentTrial = 1:MaxTrials
     switch TrialTypes(currentTrial) % Determine trial-specific state matrix fields
         case 1
            load(fullfile(BpodSystem.BpodUserPath, 'Protocols', 'PhotoStim_multi', 'LightTrain_5hz_1ms.mat'));
+           BpodSystem.Data.PulsePalProgram{currentTrial} = 'LightTrain_5hz_1ms';
         case 2
-           load(fullfile(BpodSystem.BpodUserPath, 'Protocols', 'PhotoStim_multi', 'LightTrain_10hz_1ms.mat'));            
+           load(fullfile(BpodSystem.BpodUserPath, 'Protocols', 'PhotoStim_multi', 'LightTrain_10hz_1ms.mat'));   
+           BpodSystem.Data.PulsePalProgram{currentTrial} = 'LightTrain_10hz_1ms';           
         case 3
-           load(fullfile(BpodSystem.BpodUserPath, 'Protocols', 'PhotoStim_multi', 'LightTrain_20hz_1ms.mat'));            
+           load(fullfile(BpodSystem.BpodUserPath, 'Protocols', 'PhotoStim_multi', 'LightTrain_20hz_1ms.mat')); 
+           BpodSystem.Data.PulsePalProgram{currentTrial} = 'LightTrain_20hz_1ms';           
         case 4
-           load(fullfile(BpodSystem.BpodUserPath, 'Protocols', 'PhotoStim_multi', 'LightTrain_40hz_1ms.mat'));            
+           load(fullfile(BpodSystem.BpodUserPath, 'Protocols', 'PhotoStim_multi', 'LightTrain_40hz_1ms.mat'));      
+           BpodSystem.Data.PulsePalProgram{currentTrial} = 'LightTrain_40hz_1ms';           
         case 5
-           load(fullfile(BpodSystem.BpodUserPath, 'Protocols', 'PhotoStim_multi', 'LightTrain_80hz_1ms.mat'));            
+           load(fullfile(BpodSystem.BpodUserPath, 'Protocols', 'PhotoStim_multi', 'LightTrain_80hz_1ms.mat'));
+           BpodSystem.Data.PulsePalProgram{currentTrial} = 'LightTrain_80hz_1ms';           
     end
+    disp(['*** Trial # ' num2str(currentTrial) ' Program: ' BpodSystem.Data.PulsePalProgram{currentTrial} ' ***']);
     ProgramPulsePal(ParameterMatrix);
     
    % S = BpodParameterGUI('sync', S); % Sync parameters with BpodParameterGUI plugin
     
     sma = NewStateMatrix(); % Assemble state matrix  
-    if TrialTypes(currentTrial)==1
-    sma = AddState(sma, 'Name', 'DeliverStimulus', ...
+    sma = AddState(sma, 'Name', 'Start', ...
         'Timer', 0,...
-        'StateChangeConditions', {'Tup', 'LightTrain_5hz'},...
+        'StateChangeConditions', {'Tup', 'DeliverStimulus'},...
         'OutputActions', {});
-    elseif TrialTypes(currentTrial)==2
     sma = AddState(sma, 'Name', 'DeliverStimulus', ...
-        'Timer', 0,...
-        'StateChangeConditions', {'Tup', 'LightTrain_10hz'},...
-        'OutputActions', {});
-    elseif TrialTypes(currentTrial)==3
-    sma = AddState(sma, 'Name', 'DeliverStimulus', ...
-        'Timer', 0,...
-        'StateChangeConditions', {'Tup', 'LightTrain_20hz'},...
-        'OutputActions', {});   
-    elseif TrialTypes(currentTrial)==4
-    sma = AddState(sma, 'Name', 'DeliverStimulus', ...
-        'Timer', 0,...
-        'StateChangeConditions', {'Tup', 'LightTrain_40hz'},...
-        'OutputActions', {});     
-    else
-    sma = AddState(sma, 'Name', 'DeliverStimulus', ... 
-        'Timer', 0,...
-        'StateChangeConditions', {'Tup', 'LightTrain_80hz'},...
-        'OutputActions', {});  
-    end
-    sma = AddState(sma, 'Name', 'LightTrain_5hz', ...
-        'Timer', 0,...
+        'Timer', 0.05,...
         'StateChangeConditions', {'Tup', 'ITI'},...
-        'OutputActions', {'BNCState',2});
-    sma = AddState(sma, 'Name', 'LightTrain_10hz', ...
-        'Timer', 0,...
-        'StateChangeConditions', {'Tup', 'ITI'},...
-        'OutputActions', {'BNCState',2});
-    sma = AddState(sma, 'Name', 'LightTrain_20hz', ...
-        'Timer', 0,...
-        'StateChangeConditions', {'Tup', 'ITI'},...
-        'OutputActions', {'BNCState',2});
-    sma = AddState(sma, 'Name', 'LightTrain_40hz', ...
-        'Timer', 0,...
-        'StateChangeConditions', {'Tup', 'ITI'},...
-        'OutputActions', {'BNCState',2});
-    sma = AddState(sma, 'Name', 'LightTrain_80hz', ...
-        'Timer', 0,...
-        'StateChangeConditions', {'Tup', 'ITI'},...
-        'OutputActions', {'BNCState',2});
+        'OutputActions', {'BNCState', 2});
     sma = AddState(sma, 'Name', 'ITI', ...
         'Timer', 10,...
         'StateChangeConditions', {'Tup', 'exit'},...
@@ -114,7 +77,6 @@ for currentTrial = 1:MaxTrials
         % BpodSystem.Data = BpodNotebook(BpodSystem.Data); % Sync with Bpod notebook plugin
         BpodSystem.Data.TrialSettings(currentTrial) = S; % Adds the settings used for the current trial to the Data struct (to be saved after the trial ends)
         BpodSystem.Data.TrialTypes(currentTrial) = TrialTypes(currentTrial); % Adds the trial type of the current trial to data
-        UpdateOutcomePlot(TrialTypes, BpodSystem.Data);
         SaveBpodSessionData; % Saves the field BpodSystem.Data to the current data file
     end
     
@@ -128,15 +90,3 @@ for currentTrial = 1:MaxTrials
     end
     
 end
-
-function UpdateOutcomePlot(TrialTypes, Data)
-global BpodSystem
-Outcomes = zeros(1,Data.nTrials);
-for x = 1:Data.nTrials
-    if ~isnan(Data.RawEvents.Trial{x}.States.DeliverStimulus(1))
-        Outcomes(x) = 1;
-    else
-        Outcomes(x) = 3;
-    end
-end
-OutcomePlot_PhotoStim_multi(BpodSystem.GUIHandles.OutcomePlot,'update',Data.nTrials+1,2-TrialTypes,Outcomes)
