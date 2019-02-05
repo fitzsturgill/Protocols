f61f2d56bb7179eba46650a1f1c81253bd064bff
fritzPC2\Adam <fitzsturgill@gmail.com>
2019-02-05 18:30:33 -0500

    added shock

--- FrankenLNL_4odors/rewardPunishBlocks.m
+++ FrankenLNL_4odors/rewardPunishBlocks.m
@@ -78,3 +78,29 @@ S.LinkTo = 0;
 S.LinkToFcn = '';
 
 blocks{end + 1} = S;
+
+%% block 4, SHOCK, no tone or light, reward and punish (second stage of training)
+S = struct(); ST = struct();
+ST.BlockNumber = repmat(4, 7, 1); % fluff
+ST.P = [...
+    1/3*0.8;...
+    1/3*0.2;...
+    1/3*0.8;...
+    1/3*0.2;...
+    1/3*0.45;...
+    1/3*0.45;...
+    1/3 * 0.1;...
+    ];
+ST.CS1 = [0; 0; 0; 0; 0; 0; 0;]; % will be used to select S.GUI.Odor1Valve
+ST.CS2 = [1; 1; 2; 2; 0; 0; 0;]; % will be used to select S.GUI.Odor2Valve
+ST.CS1_tone = [0; 0; 0; 0; 0; 0; 0;];
+ST.CS2_tone = [0; 0; 0; 0; 0; 0; 0;];
+ST.CS1_light = [0; 0; 0; 0; 0; 0; 0;];
+ST.CS2_light = [0; 0; 0; 0; 0; 0; 0;];
+ST.US = {'Reward'; 'Neutral'; 'Shock'; 'Neutral'; 'Reward'; 'Shock'; 'Neutral'};   % is it Rewarded ?
+S.Table = struct2table(ST);
+S.LinkTo = 0;
+S.LinkToFcn = '';
+
+blocks{end + 1} = S;
+
